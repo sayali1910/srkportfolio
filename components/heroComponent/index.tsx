1b50d086adb37f6e905f17bdead002f4bd65731b
sayali1910 <sayalikanase1910@gmail.com>
2023-04-11 18:51:24 +0530

    Add portfolio skeletone

---
+++ components/heroComponent/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./hero.module.css";
+const HeroComponent = () => {
+  return <div className={styles.heroDiv}></div>;
+};
+export default HeroComponent;
